ca6eefb10737871b2b60a10d21da8c21de01be31
tianyongping <tianyp@hxwisec.com>
2017-08-28 15:51:27 +0800

    spring cloud项目目录提交

---
+++ api-member_feign/src/main/java/me/loveshare/ApiMemberFeignApplication.java
@@ -0,0 +1,12 @@
+package me.loveshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMemberFeignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMemberFeignApplication.class, args);
+	}
+}
---
+++ api-member_ribbon/src/main/java/me/loveshare/ApiMemberRibbonApplication.java
@@ -0,0 +1,12 @@
+package me.loveshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMemberRibbonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMemberRibbonApplication.class, args);
+	}
+}
