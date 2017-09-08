3becf6e34c0127ccdf7195eb5000b15414d50aff
tianyongping <tianyp@hxwisec.com>
2017-09-08 13:44:02 +0800

    异常样式修改

--- api-member_feign/src/main/java/me/loveshare/member/configuration/FeignErrorDecoderConfiguration.java
+++ api-member_feign/src/main/java/me/loveshare/member/configuration/FeignErrorDecoderConfiguration.java
@@ -3,6 +3,7 @@ package me.loveshare.member.configuration;
 import com.netflix.hystrix.exception.HystrixBadRequestException;
 import feign.Response;
 import feign.codec.ErrorDecoder;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -12,20 +13,27 @@ import org.springframework.context.annotation.Configuration;
  * 触发熔断的异常: 服务未启动、超时时间、retry的设置、业务异常(包含java异常)。<br/>
  * 其他异常: 服务端未提供方法、IllegalArgumentException、HystrixBadRequestException等，这种异常不会纳入circuit breaker的统计里头，即不会触发熔断。<br/>
  */
+@Slf4j
 @Configuration
 public class FeignErrorDecoderConfiguration implements ErrorDecoder {
     @Override
     public Exception decode(String methodKey, Response response) {
         int status = response.status();
-        if (status >= 400 && status <= 499) {
-            StringBuilder su = new StringBuilder();
-            su.append("\n--------------------------Error--------------------------");
-            su.append("\n\tCode: ").append(response.status());
-            su.append("\n\tMessage: ").append(response.reason());
-            su.append("\n\tURL: ").append(response.request().url());
-            su.append("\n--------------------------Error--------------------------");
-            return new HystrixBadRequestException(su.toString());
+        String reason = response.reason();
+        printError(methodKey, status, reason);
+        if (status >= 400 && status <= 499) { //其他异常
+            return new HystrixBadRequestException(reason);
         }
         return feign.FeignException.errorStatus(methodKey, response);
+    }
+
+    private void printError(String methodKey, int status, String reason) {
+        StringBuilder su = new StringBuilder();
+        su.append("\n\n***** Feign Error: {");
+        su.append("\"method\":\"").append(methodKey);
+        su.append("\",\"code\":").append(status);
+        su.append(",\"reason\":\"").append(reason);
+        su.append("\"}\n");
+        log.error(su.toString());
     }
 }
